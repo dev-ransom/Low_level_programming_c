2d74237b25d80cf2feb5d991355ef6838a9eb920
Ransom Sunday <sundayyoung64@gmail.com>
2023-04-28 13:07:49 +0100

    added codes that prints alphabet in lowercase ten times

---
+++ 0x0_function/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+ * 
+ * return: x10 a-z
+*/
+
+void print_alphabet_x10(void);
+
+int main(void){
+    print_alphabet_x10();
+    return 0;
+}
+
+void print_alphabet_x10(void){
+    char a, b;
+    for (a = 0; a <= 10; a++){
+        for (b = 'a'; b <= 'z'; b++){
+            printf("%c", b);
+        }
+            printf("\n");
+    }
+}
