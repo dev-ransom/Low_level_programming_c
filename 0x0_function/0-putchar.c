08fde6461e101b009e733ff0ea91902cc0c0f7de
Ransom Sunday <sundayyoung64@gmail.com>
2023-04-28 12:28:35 +0100

    added code that prints _putchar

---
+++ 0x0_function/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - entry point
+ * 
+ * Return: always returns 0
+*/
+
+int main(){
+    fputchar('-');
+    fputchar('p');
+    fputchar('u');
+    fputchar('t');
+    fputchar('c');
+    fputchar('h');
+    fputchar('a');
+    fputchar('r');
+    return 0;
+}
